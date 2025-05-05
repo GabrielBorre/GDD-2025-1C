951656c279349443b42af674f74a70ae5831cfa6
Florencia Sanchez <marasanchez@frba.utn.edu.ar>
2025-05-05 18:20:13 -0300

    agrego las pks y un par de fks

--- SCRIPT_TP_GDD_1C_2025.sql
+++ SCRIPT_TP_GDD_1C_2025.sql
@@ -1,8 +1,11 @@
 --23 tablas
 
-CREATE SCHEMA QUERYOSOS
+CREATE SCHEMA QUERYOSOS 
+--esto me parece q no va 
 
-CREATE TABLE Pedido(nroDePedido decimal(18,0),
+
+CREATE TABLE Pedido(
+nroDePedido decimal(18,0) PRIMARY KEY,
 fechaYHora DateTime2(6),
 precioTotal decimal(18,2),
 idCliente Integer,
@@ -12,7 +15,8 @@ fechaCancelacion DateTime2(6),
 motivoCancelacion Varchar(255))
 )
 
-CREATE TABLE ItemDetallePedido (id_item_pedido Integer,
+CREATE TABLE ItemDetallePedido (
+id_item_pedido Integer PRIMARY KEY,
 nroDePedido decimal(18,0),
 sillonCodigo BigInt,
 cantidad_pedido BigInt,
@@ -20,14 +24,16 @@ subtotal decimal(18,2),
 precioUnitario decimal(18,2),
 )
 
-CREATE TABLE Factura (nroFactura BIGINT,
+CREATE TABLE Factura (
+nroFactura BIGINT PRIMARY KEY,
 idCliente Integer,
 idSucursal Integer,
 fechaYHora DateTime2(6),
 importeTotal decimal(38,2)
 )
 
-CREATE TABLE Cliente (idCliente Integer,
+CREATE TABLE Cliente (
+idCliente Integer PRIMARY KEY,
 nombre NVarchar(255),
 apellido NVarchar(255),
 nroDocumento BigInt,
@@ -37,7 +43,8 @@ telefono NVARCHAR(255),
 idDireccion Integer
 )
 
-CREATE TABLE Envio (nroDeEnvio Decimal(18,0),
+CREATE TABLE Envio (
+nroDeEnvio Decimal(18,0) PRIMARY KEY,
 nroDeFactura BigInt,
 fechaProgramada DateTime2(6),
 fechaYHoraEntrega DateTime2(6),
@@ -46,21 +53,24 @@ importeSubida decimal (18,2),
 envioTotal decimal(18,2)
 )
 
-CREATE TABLE Compra (nroDeCompra decimal(18,0),
+CREATE TABLE Compra (
+nroDeCompra decimal(18,0) PRIMARY KEY,
 idSucursal Integer,
 idProveedor Integer,
 fechaCompra dateTime2(6),
 total_compra decimal(18,2)
 )
 
-CREATE TABLE DetalleCompra(idDetalleCompra Integer,
+CREATE TABLE DetalleCompra(
+idDetalleCompra Integer PRIMARY KEY,
 precioUnitario decimal(18,2),
 cantidad decimal(18,0),
 subtotal decimal(18,2),
 idMaterial Integer
 )
 
-Create Table Proveedor (idProveedor Integer,
+Create Table Proveedor (
+idProveedor Integer PRIMARY KEY,
 nombre NVarchar(255),
 mail NVarchar(255),
 telefono NVarchar(255),
@@ -69,88 +79,107 @@ razonSocial NVarchar(255),
 cuit NVarchar(255)
 )
 
-CREATE TABLE Sillon (SillonCodigo BigInt,
+CREATE TABLE Sillon (
+SillonCodigo BigInt PRIMARY KEY,
 precioUnitario decimal(18,2),
 SillonModeloCodigo BigInt,
 idMedidaSillon Integer,
 codigo BigInt
 )
 
-Create Table Modelo (sillon_modelo_codigo BigInt,
+Create Table Modelo (
+sillon_modelo_codigo BigInt PRIMARY KEY,
 precio Decimal(18,2),
 descripcion NVarchar(255)
 )
 
-Create Table MaterialSillon(idSillon Integer,
-idMaterial Integer
-)
-
-Create Table Medida(idMedidaSillon Integer,
+Create Table Medida(
+idMedidaSillon Integer PRIMARY KEY,
 ancho decimal(18,2),
 profundidad decimal(18,2),
 alto decimal(18,2),
 precio decimal(18,2)
 )
 
-Create Table Material(idMaterial Integer,
+Create Table Material(
+idMaterial Integer PRIMARY KEY,
 tipo NVarchar(255),
 nombre NVarchar(255),
 descripcion NVarchar(255)
 )
 
-Create Table Tela(id_tela Integer,
+Create Table MaterialSillon(
+idSillon Integer,
+idMaterial Integer, 
+PRIMARY KEY(idSillon, idMaterial),
+FOREIGN KEY (idSillon) REFERENCES Sillon(SillonCodigo), --tipo de dato no coincide, o es big int o es integer 
+FOREIGN KEY (idMaterial) REFERENCES Material(idMaterial)
+)
+
+
+Create Table Tela(
+id_tela Integer PRIMARY KEY,
 IdMaterial Integer,
 telaColor NVarchar(255),
-telaTextura NVarchar(255)
+telaTextura NVarchar(255),
 precio_material decimal(18,2)
 )
 
-Create Table Madera(id_madera Integer,
+Create Table Madera(
+id_madera Integer PRIMARY KEY,
 IdMaterial Integer,
 maderaColor NVarchar(255),
 maderaDureza NVarchar(255),
 precio_material decimal(18,2)
 )
 
-Create Table Relleno(id_relleno Integer,
+Create Table Relleno(
+id_relleno Integer PRIMARY KEY,
 IdMaterial Integer,
 rellenoDensidad decimal(18,2),
 precio_material decimal(18,2)
 )
 
-Create Table Direccion(idDireccion Integer,
+Create Table Direccion(
+idDireccion Integer PRIMARY KEY,
 idLocalidad Integer,
 direccion NVarchar(255)
 )
 
-Create Table Localidad(idLocalidad Integer,
+Create Table Localidad(
+idLocalidad Integer PRIMARY KEY,
 nombre NVarchar(255),
 idProvincia Integer
 )
 
-Create Table Provincia(idProvincia Integer,
+Create Table Provincia(
+idProvincia Integer PRIMARY KEY,
 nombre NVarchar(255)
 )
 
-Create Table Sucursal(idSucursal Integer,
+Create Table Sucursal(
+idSucursal Integer PRIMARY KEY,
 numeroSucursal BigInt,
 idDireccion Integer,
 telefono NVarchar(255),
 mail NVarchar(255)
 )
 
-Create Table Motivo_cancelacion_pedido(id_motivo_cancelacion Integer,
+Create Table Motivo_cancelacion_pedido(
+id_motivo_cancelacion Integer PRIMARY KEY,
 nombre NVarchar(255)
 )
 
-Create Table Estado(idEstado Integer,
+Create Table Estado(
+idEstado Integer PRIMARY KEY,
 estado NVarchar(255),
 idPedido Integer
 )
 
-Create Table ItemDetallefactura(nroFactura BigInt,
+Create Table ItemDetallefactura(
+nroFactura BigInt PRIMARY KEY,
 id_item_pedido Integer,
-detalle_factura_precio decimal(18,2)
-detalle_factura_cantidad decimal(18,0)
+detalle_factura_precio decimal(18,2),
+detalle_factura_cantidad decimal(18,0),
 detalle_factura_subtotal decimal(18,2)
 )
